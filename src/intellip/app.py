6ca5b4410285f556a50a943558ee779785ae0d36
Sumin Seo <sumins@Suminui-MacBookAir.local>
2024-05-18 12:02:09 +0900

    add: gradio app

---
+++ src/intellip/app.py
@@ -0,0 +1,55 @@
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import gradio as gr
+
+from langchain_upstage import ChatUpstage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema import AIMessage, HumanMessage
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+UPSTAGE_API_KEY = os.getenv('UPSTAGE_API_KEY')
+
+llm = ChatUpstage(streaming=True)
+
+# More general chat
+chat_with_history_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant."),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{message}"),
+    ]
+)
+chain = chat_with_history_prompt | llm | StrOutputParser()
+def chat(message, history):
+    history_langchain_format = []
+    for human, ai in history:
+        history_langchain_format.append(HumanMessage(content=human))
+        history_langchain_format.append(AIMessage(content=ai))
+
+    generator = chain.stream({"message": message, "history": history_langchain_format})
+
+    assistant = ""
+    for gen in generator:
+        assistant += gen
+        yield assistant
+with gr.Blocks() as demo:
+    chatbot = gr.ChatInterface(
+        chat,
+        examples=[
+            "How to eat healthy?",
+            "Best Places in Korea",
+            "How to make a chatbot?",
+        ],
+        title="Solar Chatbot",
+        description="Upstage Solar Chatbot",
+    )
+    chatbot.chatbot.height = 300
+if __name__ == "__main__":
+    demo.launch()
